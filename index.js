b2680f86f1ac8c5ea62ab0b13032bd44e14dc000
Giona Righini <sapbasis@alteanet.it>
2021-01-23 13:52:01 +0100

    ...

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const vision = require("@google-cloud/vision")
 
 const { createWorker } = require("tesseract.js")
 
+const mobilenet = require("@tensorflow-models/mobilenet")
+
+//const img = document.getElementById('img');
+
 // client tesseract
 const worker = createWorker()
 
-// Creates a client g
+// Creates a client googleVision
 const client = new vision.ImageAnnotatorClient()
 
 const fileName = "res/passport.jpg"
@@ -17,6 +21,14 @@ const app = async () => {
   console.log("Text:")
   //detections.forEach((text) => console.log(text))
   console.log(detections[0])
+
+  // TensorFlow - Classify the image
+  const model = await mobilenet.load()
+
+  const predictions = await model.classify(fileName)
+
+  console.log("Predictions: ")
+  console.log(predictions)
 
   // tesseract - lentissimo
   await worker.load()
